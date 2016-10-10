08ed8de8fd31486984f12e96c047dead45b1ca39
Pavel F <fedoseevpavel@gmail.com>
2016-10-10 22:59:33 +0600

    I really love Tanya

--- src/com/tele2/Main.java
+++ src/com/tele2/Main.java
@@ -2,6 +2,10 @@ package com.tele2;
 
 public class Main{
 	public static void main(String[] args){
-		System.out.println("I love you Tanya");
+		
+		
+		for (int i=0; i<10;i++){
+			System.out.println("I love you Tanya");
+		}
 	}
 }
