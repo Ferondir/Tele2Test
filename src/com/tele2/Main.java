30bb3347a04f888a3bf03a22800b022820edc256
Pavel F <fedoseevpavel@gmail.com>
2016-10-11 22:56:27 +0600

    add arrayPrint

--- src/com/tele2/Main.java
+++ src/com/tele2/Main.java
@@ -4,6 +4,10 @@ public class Main{
 	public static void main(String[] args){
 		
 		
+		int[] array = {1,5,1,0,0,2,7,7,2,5};
+		
+		Tele2Array.arrayPrint(array);
+		
 		for (int i=0; i<10;i++){
 			System.out.println("I love you Tanya");
 		}
---
+++ src/com/tele2/Tele2Array.java
@@ -0,0 +1,9 @@
+package com.tele2;
+
+public class Tele2Array{
+		public static void arrayPrint(int[] array){
+			for (int e : array){
+					System.out.print(e + " ");
+			}
+		}
+}
