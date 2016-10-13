28d6063e6ebb71b6c602e3e32c2384613bb96184
Pavel Fedosseyev <fedoseevpavel@gmail.com>
2016-10-13 18:54:20 +0600

    Add sortBubble / sot Slelection

--- src/com/tele2/Main.java
+++ src/com/tele2/Main.java
@@ -11,12 +11,23 @@ public class Main{
 		MyArray.arrayReverse(array);
 		MyArray.arrayPrint(array);
 		
-		MyArray.arrayFill(array);
+		//MyArray.arrayFill(array);
+		MyArray.arrayPrint(array);
+		
+		MyArray.arraySortBubble(array);
+		MyArray.arrayPrint(array);
+		
+		int[] array2 = {1,5,1,0,0,2,7,7,2,5};
+		MyArray.arrayPrint(array2);
+		
+		MyArray.arraySortSelection(array);
 		MyArray.arrayPrint(array);
 		
 		
+		/*
 		for (int i=0; i<10;i++){
 			System.out.println("I love you Tanya");
 		}
+		*/
 	}
 }
--- src/com/tele2/MyArray.java
+++ src/com/tele2/MyArray.java
@@ -39,6 +39,37 @@ public class MyArray{
 	
 	public static int[] arrayShuffle(int[] ar){
 		
+		
+		
+		return(ar);
+	}
+	
+	public static int[] arraySortBubble(int[] ar){
+		
+		for(int i=0;i<ar.length;i++){
+			for(int j=0;j<ar.length-i-1;j++){
+				if(ar[j]>ar[j+1]) arraySwapElement(ar,j,j+1);
+			}
+		}
+		
+		return(ar);
+	}
+	
+	public static int[] arraySortSelection(int[] ar){
+		
+		for(int i=0;i<ar.length-1;i++){
+			
+			int max_index = i;
+			
+			for(int j=0;i<ar.length-i;j++){
+				if(ar[max_index] > ar[j]){
+					max_index = j;
+				}
+			}
+			
+			arraySwapElement(ar,max_index,i);
+		}
+		
 		return(ar);
 	}
 	
